fb84582330ef35eb6c3d0386a44c136d46188a39
nada <nada.mesbah@usmba.ac.ma>
2023-04-30 07:56:00 +0100

    controller upd

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param } from '@nestjs/common';
 
 @Controller('ninjas')
-export class NinjasController {}
+export class NinjasController {
+  //GET /ninjas/
+  //GET /ninjas?type=fast --> we use @Query('type') type: string
+  @Get()
+  getNinjas() {
+    return [];
+  }
+  //GET /ninjas/:id
+  @Get(':id')
+  getOneNinja(@Param('id') id: string) {
+    return {
+      id,
+    };
+  }
+  //POST /ninjas
+  @Post()
+  createNinja() {
+    return {};
+  }
+  //PUT /ninjas/:id
+  @Put(':id')
+  updateNinja() {
+    return {};
+  }
+  //DELETE /ninjas/:id
+  @Delete(':id')
+  deleteNinja() {
+    return {};
+  }
+}
